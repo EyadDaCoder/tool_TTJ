77ab7262ae01fe582348d04b2a842a1f80676997
Eyad Taha <eyadta84@gmail.com>
2025-07-24 14:59:35 +0400

    Add files via upload

---
+++ tool-TTJ.py
@@ -0,0 +1,11 @@
+# made by the one and only eyad taha
+
+print('tool_TTJ %%% Tool Text To JSON')
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+exp = input("Enter expression: ")
+
+json = f'{{"n1": {n1}, "n2": {n2}, "exp": "{exp}" }}'
+print("Copy and paste this code into Node_RED:")
+print(json)
